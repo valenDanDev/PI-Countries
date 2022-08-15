811c9bb201d9839de698d12f5925a446d3a2e486
unknown <valenciadaniel8022015@gmail.com>
2022-08-15 12:51:08 -0500

    navigacion

--- client/src/components/Navigation/Navbar.jsx
+++ client/src/components/Navigation/Navbar.jsx
@@ -1,31 +1,20 @@
 import { Link, NavLink } from 'react-router-dom';
 import styles from "./Navbar.module.css"
-//import Filter from './Filters';
-
 
 export default function Navbar (){
-
     return (
         <nav className={styles.nav_container}>
-        <div className={styles.navigation}>
-            
+        <div className={styles.navigation}>          
             <Link className={styles.logo} to="/home">
                 Countries
             </Link>
-        
               <div className={styles.navbar}>
               <NavLink 
                   className={ ({isActive}) => `link  ${ isActive ? 'active':'' }` }
                   to="/activitiesCreate"
               >  Create activities   </NavLink>  
-            
-
           </div>
-     
-                
         </div>
-       
-          
         </nav>
     );
 }
