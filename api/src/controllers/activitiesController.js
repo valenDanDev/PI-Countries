7c1d9a22add2e5d6521dc4fd998c8a68813a3a96
unknown <valenciadaniel8022015@gmail.com>
2022-11-19 14:22:55 -0500

    back fix

--- api/src/controllers/activitiesController.js
+++ api/src/controllers/activitiesController.js
@@ -2,103 +2,114 @@ const {Activities,Country,countries_activities} = require("../db.js");
 
 const postActivities = async (req, res) =>{
     const { name,dificulty,duration,season,country } = req.body
-
+    var i=0;
+  while(country.length>i){    
+    var currenC=country[i]
+    console.log(currenC)
+    i++;
     try {
-        if (!name || !dificulty || !duration || !season ){
-            return res.status(404).send("You need more information!!")
-        }
-        if(dificulty<1 || dificulty>5){
-            return res.status(404).send("the dificulty of the activity must be between 1 and 5!!")
-        }
-       /* const seasonA=season
-       var i=0
-       for ( i=0;  i<seasonA.length;i++){
-           if(seasonA[i]!=="summer" && seasonA[i]!=="spring" ){
-            return res.status(404).send("you must type one of  the four seasons (summer,autumn,winter,spring)!!")
-           }
-           else{
-            console.log('succefully')
-           }
-        }
-        console.log(seasonA);*/
-        /*if(season.map(v=>{
-          v!=="summer" 
-        }) 
+      if (!name || !dificulty || !duration || !season ){
+          return res.status(404).send("You need more information!!")
+      }
+      if(dificulty<1 || dificulty>5){
+          return res.status(404).send("the dificulty of the activity must be between 1 and 5!!")
+      }
+     /* const seasonA=season
+     var i=0
+     for ( i=0;  i<seasonA.length;i++){
+         if(seasonA[i]!=="summer" && seasonA[i]!=="spring" ){
+          return res.status(404).send("you must type one of  the four seasons (summer,autumn,winter,spring)!!")
+         }
+         else{
+          console.log('succefully')
+         }
+      }
+      console.log(seasonA);*/
+      /*if(season.map(v=>{
+        v!=="summer" 
+      }) 
+      
+
+      ){
         
-
-        ){
-          
-            return res.status(202).send("you must type one of  the four seasons (summer,autumn,winter,spring)!!")
-        }*/
-          let checkActivity = await Activities.findAll({
-            where: {
-              name: name,
-            },
-          });
-         const countrymatch = await Country.findAll({
-            attributes: ['id'],
-            where: {
-              name: country.toLowerCase(),
-            },
-          });
-         
-          if(!Object.keys(countrymatch).length){
-            return res.status(404).json({
-              msg: `you need to choose a country before of creating the activity`
-          })
-          }
-          let result2,actId=0;
-          let result = countrymatch.map(a => a.id).toString();
-
-          //validate if an activity exists in the country
-
-          let activitymatch= await Activities.findAll({
-            attributes: ['id'],
-            where: {
-              name:name.toLowerCase().trim()
-            }
-           }
-           );
-           if(Object.keys(activitymatch).length){
-            result2 = activitymatch.map(a => a.id);
-              actId=(result2.pop()).toString();
-           }
+          return res.status(202).send("you must type one of  the four seasons (summer,autumn,winter,spring)!!")
+      }*/
+        let checkActivity = await Activities.findAll({
+          where: {
+            name: name,
+          },
+        });
+       const countrymatch = await Country.findAll({
+          attributes: ['id'],
+          where: {
+            id: currenC,
+          },
+        });
+      
+        if(!Object.keys(countrymatch).length){
+          return res.status(404).json({
+            msg: `you need to choose a country before of creating the activity`
+        })
+        }
+        let result2,actId=0;
+        let result = countrymatch.map(a => a.id).toString();
         
-          const activityIncountry = await countries_activities.findAll({
-            attributes: ['activityId'],
-            where: {
-              countryId: result,
-              activityId: actId
-            },
-          });
+        //validate if an activity exists in the country
 
-         // console.log(Object.keys(activityIncountry).length);
-
-          if(Object.keys(checkActivity).length && Object.keys(activityIncountry).length){
-            return res.status(404).json({
-              msg: "this activity already exists in this country"
-          })
+        let activitymatch= await Activities.findAll({
+          attributes: ['id'],
+          where: {
+            name:name.toLowerCase().trim()
           }
-         await Activities.create({name, dificulty, duration, season})
+         }
+         );
+         if(Object.keys(activitymatch).length){
+          result2 = activitymatch.map(a => a.id);
+            actId=(result2.pop()).toString();
+         }
+        const activityIncountry = await countries_activities.findAll({
+          attributes: ['activityId'],
+          where: {
+            countryId: result,
+            activityId: actId
+          },
+        });
+
+        
+       // console.log(Object.keys(activityIncountry).length);
+
+        if(Object.keys(checkActivity).length && Object.keys(activityIncountry).length){
+          console.log("this activity already exists in " + currenC);
+        }
+        else{
+          if(activitymatch.length===0){
+            await Activities.create({name, dificulty, duration, season})
           activitymatch= await Activities.findAll({
             attributes: ['id'],
             where: {
               name:name
             }
            }
-           );
-           result2 = activitymatch.map(a => a.id);
-           actId=(result2.pop()).toString();    
-        //console.log(actId);    
-         let obj1 = {
-          countryId: result,
-          activityId: actId
-       }  
-         await countries_activities.create(obj1)
-        return res.status(200).send("activity created succesfully")
-       } catch (error) {
-        return res.status(404).send(error.message)
-    }
+           ); 
+          }
+          result2 = activitymatch.map(a => a.id);
+          actId=(result2.pop()).toString();    
+       //console.log(actId);    
+        let obj1 = {
+         countryId: result,
+         activityId: actId
+      }  
+        await countries_activities.create(obj1)
+        }
+
+       
+      
+     } catch (error) {
+      return res.status(404).send(error.message)
+  }
+
+  }
+  return res.status(200).send("activities created succesfully in the selected")
 }
 
 const getallactivities= async (req,res)=>{
