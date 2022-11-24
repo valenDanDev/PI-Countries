b685b5d1483a4f3c7e80cd0fe66ec24fda91b8af
unknown <valenciadaniel8022015@gmail.com>
2022-11-23 19:16:33 -0500

    not found

--- client/src/components/Error404/NotFound404.jsx
+++ client/src/components/Error404/NotFound404.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import Navbar from '../Navigation/Navbar'
+import styles from './NotFound404.module.css'
 
 function NotFound404() {
   return (
-    <div>
+    <div >
         <Navbar/>
-        <div >
-            <h1 >404</h1>
-            <h2>Page not found</h2>
-            <p>The page you are looking for does not exist</p>
-            <p>Go back to</p>
-            <Link to="/home">
-                <button >Home</button>
+        <div className={styles.container}>
+          <div>
+          <div className={styles.details_container} > 
+             <h2 className={styles.notfound_t} >404</h2>
+             <h2 className={styles.notfound_t}>Page not found</h2>
+             <p className={styles.notfound_te}>The page you are looking for does not exist</p>
+             <p className={styles.image_container}>
+            <img src='./img/countries.png' alt="imgn"></img>
+          </p>
+             <p className={styles.notfound_te}>Go back to</p>
+              <Link to="/home">
+                <button className={styles.card_d}>Home</button>
             </Link>
+        </div> 
+          </div>
+   
         </div>
     </div>
   )
